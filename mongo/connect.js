30b4146aa74e7011a259de6cc3563ad011d7b7ad
Vaishakh GK <vaishakhgk2006@gmail.com>
2024-12-14 14:08:35 +0530

    feat: Connection for Mongo Db

---
+++ mongo/connect.js
@@ -0,0 +1,28 @@
+import { MongoClient, ServerApiVersion } from "mongodb"
+
+const uri = "mongodb://127.0.0.1:27017"
+
+export const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+
+export let db;
+
+export const mongoStart = async () => {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    db = client.db("farm-direct");
+    // Send a ping to confirm a successful connection
+    await client.db("farm-direct").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 import express from "express";
+import { mongoStart, client } from "./mongo/connect.js"
 
 const app = express();
+
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from Express!");
 });
 
-app.listen(PORT, () => {
+mongoStart().catch(console.dir);
+
+var server = app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
+
+const gracefulShutdown = async () => {
+    console.log("Shutting down server...");
+    await client.close();
+    server.close(() => {
+        console.log("Express server stopped.");
+        process.exit(0);
+    });
+};
+
+// Handle shutdown signals
+process.on("SIGINT", gracefulShutdown); // Ctrl+C
+process.on("SIGTERM", gracefulShutdown); // Termination signal
+process.on("exit", gracefulShutdown); // On process
